8d5deade7902c5416738d0effc9a64ce2169a400
Fabian Kosmale <0inkane@googlemail.com>
2012-02-16 23:41:19 +0100

    finished getmirrors.sh

--- getmirrors.sh
+++ getmirrors.sh
@@ -11,8 +11,9 @@ tmpfile=$(mktemp --suffix=-mirrorlist)
 # Get latest mirror list and save to tmpfile
 wget -qO- "$url" | sed 's/^#Server/Server/g' > "$tmpfile"
 
-# some sed magic: get all lines including server, drop all but the first
-# x86-64 works for all repos, i684 won't work with multilib
+# some sed magic: get all lines containing "server", drop all but the first
+# x86-64 works for all repos, i686 won't work with multilib
 server=$(sed -n '/Server/p' $tmpfile | head -1 | sed 's/$arch/x86-64/g')
 
-echo $server 
+sed -i 's|= [^ ]*|= '"$server"'|g' ./archrepos.pacman.conf 
+
