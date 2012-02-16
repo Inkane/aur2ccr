53ae0f98fbea3ce4257227a5b26f25acd23427c6
Fabian Kosmale <0inkane@googlemail.com>
2012-02-16 22:20:48 +0100

    added a script to find nearest mirro

---
+++ getmirrors.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# modified script, orginally from the Arch linux forum
+
+[ "$UID" != 0 ] && su=sudo
+
+country='United+States' # replace this with your country
+url="http://www.archlinux.org/mirrorlist/?country=$country&protocol=ftp&protocol=http&ip_version=4&use_mirror_status=on"
+
+tmpfile=$(mktemp --suffix=-mirrorlist)
+
+# Get latest mirror list and save to tmpfile
+wget -qO- "$url" | sed 's/^#Server/Server/g' > "$tmpfile"
+
+# Backup and replace current mirrorlist file
+{ echo "Backing up the original mirrorlist..."
+  $su mv -i /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.orig; } &&
+	  { echo "Rotating the new list into place..."
+	    $su mv -i "$tmpfile" /etc/pacman.d/mirrorlist; }
