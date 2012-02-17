b8411334733ea61333583bcf30267e5a14625b00
george <rpubaddr0@gmail.com>
2012-02-17 09:49:07 -0700

    added -s to -unstable, added no-such-country checking and configurable archrepos.pacman.conf-location to getmirrors.sh

--- getmirrors.sh
+++ getmirrors.sh
@@ -2,14 +2,18 @@
 # modified script, orginally from the Arch linux forum
 
 country="${country-Germany}" # replace this with your country
+apconf="${apconf-./archrepos.pacman.conf}"
 url="http://www.archlinux.org/mirrorlist/?country=$country&protocol=ftp&protocol=http&ip_version=4&use_mirror_status=on"
 tmpfile=$(mktemp --suffix=-mirrorlist)
 
 # Get latest mirror list and save to tmpfile
 wget -qO- "$url" | sed 's/^#Server/Server/g' > "$tmpfile"
 
+# Check for invalid countries
+grep -o " $country is not one of the available choices." "$tmpfile" && exit 1
+
 # some sed magic: get all lines containing "server", drop all but the first
 # x86-64 works for all repos, i686 won't work with multilib
 server=$(sed -n 's/^Server = //p' $tmpfile | head -1 | sed 's/$arch/x86-64/g')
 
-sed -i 's|= [^ ]*|= '"$server"'|g' ./archrepos.pacman.conf 
+sed -i 's|= [^ ]*|= '"$server"'|g' "$apconf"
