52281b22df188eceeeac72e13e14c160640304ba
Fabian Kosmale <0inkane@googlemail.com>
2012-02-17 21:33:33 +0100

    finished country detection, needs testing

--- getmirrors.sh
+++ getmirrors.sh
@@ -4,9 +4,11 @@
 # determine the location of the user via a website
 $(wget -qiet --referer="http://www.google.com" --user-agent="Mozilla/5.0 (Windows; U;Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6" --header="Accept:text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5" --header="Accept-Language: en-us,en;q=0.5" --header="Accept-Encoding: gzip,deflate"  --header="Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7" --header="Keep-Alive: 300" "$@" -O lang.gz http://www.ip-adress.com/)
 gunzip lang.gz
-echo $(sed -n 's/flag/&/p' <lang)
+coun=$(echo $(sed -n 's/flag/&/p' <lang) |  sed 's/.\+flag"> //;s/ <br.\+//;')
 rm lang
-country="${country-Germany}" # replace this with your country
+
+
+country="${country-$coun}" 
 apconf="${apconf-./archrepos.pacman.conf}"
 url="http://www.archlinux.org/mirrorlist/?country=$country&protocol=ftp&protocol=http&ip_version=4&use_mirror_status=on"
 tmpfile=$(mktemp --suffix=-mirrorlist)
