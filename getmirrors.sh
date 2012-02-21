2b65c7fe048c3b6d2d4d18378ce8f6d3ae1d328a
george <rpubaddr0@gmail.com>
2012-02-21 10:03:56 -0700

    changed wget -qO - to wget -qO-

--- getmirrors.sh
+++ getmirrors.sh
@@ -2,7 +2,7 @@
 # modified script, orginally from the Arch linux forum
 
 # determine the location of the user via a duckduckgo request
-coun="$(wget -qO - "https://duckduckgo.com/lite/?q=ip" | grep "(your IP\ address)" | sed 's/.*(your IP address) in: .*, \(.*\s*.*\)\..*/\1/; s/ (.*)//')"
+coun="$(wget -qO- "https://duckduckgo.com/lite/?q=ip" | grep "(your IP\ address)" | sed 's/.*(your IP address) in: .*, \(.*\s*.*\)\..*/\1/; s/ (.*)//')"
 [[ "$quiet" == 1 ]] && (echo $coun; exit 0) && exit 0
 [[ -z "$coun"  ]] && coun=Any # backup, in case autodetect fails
 
