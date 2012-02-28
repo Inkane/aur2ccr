7bbec7155f3f0f062b11cc2f0b9bbf9caeb972de
Fabian Kosmale <0inkane@googlemail.com>
2012-02-28 21:23:42 +0100

    +basic features for getmirrors.py

--- getmirrors.py
+++ getmirrors.py
@@ -1,41 +1,103 @@
 #!/usr/bin/env python2
+# inspired by a bash script from Arch 
 import sys
-import os
 import urllib2
 import contextlib
 import re
 
-countries = dict()
-countries["United Kingdom"] = "Great Britain"
+class SmartDict(dict):
+    """return 'Any' as fallback if country is not in list"""
+
+    def __missing__(self, key):
+        return 'Any'
+
+
+valid_countries = ['Any',
+ 'Australia',
+ 'Belarus',
+ 'Belgium',
+ 'Brazil',
+ 'Bulgaria',
+ 'Canada',
+ 'Chile',
+ 'China',
+ 'Colombia',
+ 'Czech',
+ 'Denmark',
+ 'Estonia',
+ 'Finland',
+ 'France',
+ 'Germany',
+ 'Great Britain',
+ 'Greece',
+ 'Hungary',
+ 'India',
+ 'Indonesia',
+ 'Ireland',
+ 'Israel',
+ 'Italy',
+ 'Japan',
+ 'Kazakhstan',
+ 'Korea',
+ 'Latvia',
+ 'Luxembourg',
+ 'Macedonia',
+ 'Netherlands',
+ 'New',
+ 'Norway',
+ 'Poland',
+ 'Portugal',
+ 'Romania',
+ 'Russia',
+ 'Singapore',
+ 'Slovakia',
+ 'South',
+ 'Spain',
+ 'Sweden',
+ 'Switzerland',
+ 'Taiwan',
+ 'Turkey',
+ 'Ukraine',
+ 'United States',
+ 'Uzbekistan']
+alt_country_names = SmartDict() # store alternate country names
+alt_country_names["United Kingdom"] = "Great Britain"
 duckduckgo = "https://duckduckgo.com/lite/?q=ip"
 archlinux = "http://www.archlinux.org/mirrorlist/?country={}&protocol=ftp&protocol=http&ip_version=4&use_mirror_status=on"
 
 def download(url):
     webfile = urllib2.urlopen(url)
-    return webfile
+    return contextlib.closing(webfile)
 
-def main():
-    # get location of the user
-    with contextlib.closing(download(duckduckgo)) as coun_file:
+def get_location():
+    with download(duckduckgo) as coun_file:
         country = ""
         for line in coun_file:
             if "(your IP address)" in line:
                 country =  re.search(",\s(([a-zA-Z])+?)\.", line).group(1)
                 break
-    if country:
-        url = archlinux.format(countries[country] if country in countries else
-                    country)
-    else:
-        url = ""
-    with contextlib.closing(download(url)) as mirrorfile:
+    if country not in valid_countries:
+        country = alt_country_names[country]
+    return country
+
+def main():
+    country = get_location()
+    #create the fitting url
+    url = archlinux.format(country)
+    print url
+    with download(url) as mirrorfile:
         for line in mirrorfile:
+            if "is not one of the available choiches" in line:
+                print "Something went wrong"
+                sys.exit(1)
             tmp = re.match("\#(Server.*)",line)
             if tmp:
-                print tmp.group(1)
-                #break
-            #else:
-                #continue
-            print line
+                # replace $arch with x86_64
+                mirror = re.sub("\$arch","x86_64",tmp.group(1))
+    if mirror:
+        print mirror
+    else:
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
