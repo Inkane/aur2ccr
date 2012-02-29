6b7c03080674b138d9c6199d87b18f8c344f59d5
Fabian Kosmale <0inkane@googlemail.com>
2012-02-29 01:31:12 +0100

    getmirrors.py should work now work for the United States

--- getmirrors.py
+++ getmirrors.py
@@ -26,7 +26,7 @@ try:
 except KeyError:
     quiet = False
 
-
+# all countries accepted by the arch website
 valid_countries = [
  'Australia', 'Belarus',  'Belgium',  'Brazil',
  'Bulgaria',  'Canada',  'Chile',  'China',
@@ -42,10 +42,15 @@ valid_countries = [
  ]
 alt_country_names = SmartDict() # store alternate country names
 alt_country_names["United Kingdom"] = "Great Britain"
+
+# the webadresses of duckduckgo and arch linux 
 duckduckgo = "https://duckduckgo.com/lite/?q=ip"
+# American proxy
+#duckduckgo2 = "http://www.americanproxy.org/browse.php?u=http%3A%2F%2Fduckduckgo.com%2Flite%2F%3Fq%3Dip&b=0" 
 archlinux = "http://www.archlinux.org/mirrorlist/?country={}&protocol=ftp&protocol=http&ip_version=4&use_mirror_status=on"
 
 def download(url):
+    # downloads a file and returns a file like object if successful
     try:
         webfile = urllib2.urlopen(url)
     except (urllib2.URLError, urllib2.HTTPError):
@@ -55,11 +60,17 @@ def download(url):
     return contextlib.closing(webfile)
 
 def get_location():
+    regex_country = re.compile(r"""
+            ,\s # a comma followed by whitespace
+              ((?P<oneword>([a-zA-Z])+?)\.) # one-word countries
+            | (?P<twoword>(([a-zA-Z])+?\s[a-zA-Z]+))((\s([,(]))) #two-word countries
+            """, re.VERBOSE)
     with download(duckduckgo) as coun_file:
         country = ""
         for line in coun_file:
             if "(your IP address)" in line:
-                country =  re.search(",\s(([a-zA-Z])+?)\.", line).group(1)
+                result =  re.search(regex_country, line).groupdict()
+                country = result["oneword"] if result["oneword"] else result["twoword"]
                 break
     if country not in valid_countries:
         country = alt_country_names[country]
