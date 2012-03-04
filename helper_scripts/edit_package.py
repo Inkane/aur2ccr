e09b063263bd9941e427c28d26e0a95e92405c1f
Fabian Kosmale <0inkane@googlemail.com>
2012-03-04 17:55:50 +0000

    completed first test

--- helper_scripts/edit_package.py
+++ helper_scripts/edit_package.py
@@ -3,11 +3,13 @@
 # reimplementing edit_package.sh
 from __future__ import print_function
 from __future__ import unicode_literals
+import logging
 
 import re
 import fileinput
 
 def addmaintainer(pkgbuild, maintainer):
+    logging.info("adding maintainer to {}".format(pkgbuild))
     # replace previous maintainer
     regex = re.compile(r"# Maintainer:")
     flag = True
@@ -16,6 +18,10 @@ def addmaintainer(pkgbuild, maintainer):
             print("# Maintainer: {}".format(maintainer))
             flag = not flag
         if re.match(regex, line):
-            print("match")
-        print(re.sub(regex, "# Contributor:", line), end="")
+            print(re.sub(regex, "# Contributor:", line), end="")
+        else:
+            print(line, end="")
 
+def update_uchksums(pkbuild):
+    # update the checksum array of a PKGBUILD
+    pass
--- helper_scripts/test_edit_package.py
+++ helper_scripts/test_edit_package.py
@@ -2,21 +2,39 @@
 # -*- coding: utf-8 -*-
 # unit test
 
-import edit_package
+from __future__ import print_function
+
+import os
+import sys
 import unittest
 import tempfile
+import filecmp
 import shutil
+import edit_package
 
 class Addmaintainer(unittest.TestCase):
 
-    base_file = "./PKGBUILD"
-    compare_file = "./PKGBUILD_addmaintainer"
+    base_dir = "./pkgbuilds/"
+    compare_dir = "after_addmaintainer"
+    maintainer = "Unittest <test@example.com>"
+    workaround = "/home/{}/tmp.txt".format(os.environ["USER"])
     
-    def create_test_file(self, file):
-        f = tempfile.TemporaryFile()
-        with open(base_file) as src:
-            shutil.copyfile(src, f)
-        return f
+    def create_test_file(self, path_to_file):
+        #tmpf = tempfile.NamedTemporaryFile(mode="w+t")
+        #shutil.copy(path_to_file, tmpf.name)
+        #return tmpf
+        shutil.copy(path_to_file, self.workaround)
+        return self.workaround
     
     def test_addmaintainer_nochange_rest(self):
-        e
+        root, tmp, files = next(os.walk(self.base_dir))
+        root = os.path.abspath(root)
+        for file in files:
+            tempf = self.create_test_file(os.path.join(root,file))
+            edit_package.addmaintainer(tempf, self.maintainer)
+            cmp = os.path.join(root,self.compare_dir,file)
+            assert filecmp.cmp(tempf, cmp), "Files are not equal. Error in {}".format(file)
+
+
+if __name__ == "__main__":
+    unittest.main()
