e3732e804e5233c8667d5bd4d0338ea0e5a68959
Fabian <0inkane@googlemail.com>
2012-03-17 00:51:43 +0100

    completed base pareser, time for utility functions

--- pkgbuild_parser.py
+++ pkgbuild_parser.py
@@ -1,91 +1,86 @@
-import pyparsing as pypar
-from pyparsing import Word
-from pyparsing import Literal
+from pyparsing import Word, OneOrMore, Literal, alphanums, Optional, oneOf, nums, Group, alphas, quotedString, printables, ZeroOrMore, Combine, nestedExpr, lineEnd
+import logging
 
-compare_operators = pypar.oneOf("< > =  >= <=")
-valname = pypar.alphanums+"-_"
+def opQuotedString(pattern):
+    return quotedString(pattern) | Word (pattern)
+
+compare_operators = oneOf("< > =  >= <=")
+valname = alphanums+"-_"
 
 # version number
-vnum = Word(pypar.nums) + pypar.Optional(Word(pypar.nums + "."))
+vnum = Word(nums) + Optional(Word(nums + "."))
 
 # a valid name for a package
-package = Word(pypar.alphas + "".join((valname,".")))
+val_package_name = Combine(Word(alphas + "".join((valname,"."))))
 
-# match the pkgname variable
-pkgname = Literal("pkgname=") + package
+pkgname = Literal("pkgname=") + val_package_name
 
-# the pkgver variable
 pkgver = Literal("pkgver=") + vnum
 
-# the pkgrel variable 
-pkgrel = Literal("pkgrel=") + Word(pypar.nums)
+pkgrel = Literal("pkgrel=") + Word(nums)
 
-# the epoch variable
-epoch = Literal("epoch=") + Word(pypar.nums)
+epoch = Literal("epoch=") + Word(nums)
 
-# the pkgdesc variable
-pkgdesc = Literal("pkgdesc=") + pypar.quotedString(valname) | Word(valname)
+pkgdesc = Literal("pkgdesc=") + opQuotedString(valname)
 
 # define a valid architecture
-valid_arch = pypar.quotedString(valname) | Word(valname)
+valid_arch = opQuotedString(valname)
 
-# the arch variable
-arch = Literal("arch=(") + pypar.OneOrMore(valid_arch) + ")"
+arch = Literal("arch=(") + OneOrMore(valid_arch) + ")"
 
-# the url variable
 # TODO replace it with a better url parser
-url = Literal("url=") + pypar.quotedString(pypar.printables)
+url = Literal("url=") + quotedString(printables)
 
-# the license variable
-license = Literal("license=(") + pypar.OneOrMore(valname) + ")"
+license = Literal("license=(") + OneOrMore(valname) + ")"
 
-# the groups variable
-groups = Literal("groups=(") + pypar.OneOrMore(valname) + ")"
+groups = Literal("groups=(") + OneOrMore(valname) + ")"
 
-# a dependency is a quoted pkname with optional comparision of version numer
-dependency = "'" + package + pypar.Optional(compare_operators + vnum) + "'" | '"' + package + pypar.Optional(compare_operators + vnum) + '"'
+dependency = "'" + val_package_name + Optional(compare_operators + vnum) + "'" | '"' + val_package_name + Optional(compare_operators + vnum) + '"'
 
-# the depends variable
-depends = Literal("depends=(") + pypar.ZeroOrMore(dependency) + ")"
+depends = Literal("depends=(") + ZeroOrMore(dependency) + ")"
 
-# the makedepends variable
-makedepends = Literal("makedepends=(") + pypar.ZeroOrMore(dependency) + ")"
+makedepends = Literal("makedepends=(") + ZeroOrMore(dependency) + ")"
 
-# the optdepends variable
-optdepends = Literal("optdepends=(") + pypar.ZeroOrMore(dependency) + ")"
+optdepends = Literal("optdepends=(") + ZeroOrMore(dependency) + ")"
 
-# the checkdepends variable
-checkdepends = Literal("checkdepends=(") + pypar.ZeroOrMore(dependency) + ")"
+checkdepends = Literal("checkdepends=(") + ZeroOrMore(dependency) + ")"
 
-# the provides variable, need some checking to disallow pckgname>3.2
-provides = Literal("provides=(") + pypar.ZeroOrMore(dependency) + ")"
+provides = Literal("provides=(") + ZeroOrMore(dependency) + ")"
 
-conflicts = Literal("conflicts=(") + pypar.ZeroOrMore(dependency) + ")"
+conflicts = Literal("conflicts=(") + ZeroOrMore(dependency) + ")"
 
-replaces = Literal("replaces=(") + pypar.ZeroOrMore(dependency) + ")"
+replaces = Literal("replaces=(") + ZeroOrMore(dependency) + ")"
 
-backup = Literal("backup=(") + pypar.ZeroOrMore(valname) + ")"
+backup = Literal("backup=(") + ZeroOrMore(valname) + ")"
 
-valid_options = pypar.oneOf("strip docs libtool emptydirs zipman ccache"
+valid_options = oneOf("strip docs libtool emptydirs zipman ccache"
                             "distcc buildflags makeflags")
-options = Literal("options=(") + pypar.ZeroOrMore(valid_options) + ")"
+options = Literal("options=(") + ZeroOrMore(valid_options) + ")"
 
-install = Literal("install=") + pypar.ZeroOrMore(pypar.quotedString(valname)
-                                                | Word(valname))
+install = Literal("install=") + ZeroOrMore(opQuotedString(valname))
 
-changelog = Literal("changelog=") + pypar.ZeroOrMore(pypar.quotedString(valname)
-                                                | Word(valname))
+changelog = Literal("changelog=") + ZeroOrMore(opQuotedString(valname))
+
 # TODO better parsing, allow filename::url, but forbid fi:lename
-source = Literal("source=(") + pypar.ZeroOrMore(pypar.quotedString(valname+"$:")
-                                                | Word(valname+"$:")) + ")"
+source = Literal("source=(") + ZeroOrMore(opQuotedString(valname+"$:")) + ")"
 
-noextract = Literal("noxetract=(") +pypar.ZeroOrMore(pypar.quotedString(valname)
-                                                     | Word(valname))
-valid_chksums = pypar.oneOf("sha1sums sha256sums sha384sums sha512sums md5sums")
-chksums = valid_chksums + "=(" + pypar.ZeroOrMore(pypar.quotedString(
-                                        pypar.alphanums) | pypar.alphanums) + ")"
+noextract = Literal("noxetract=(") +ZeroOrMore(opQuotedString(valname))
+valid_chksums = oneOf("sha1sums sha256sums sha384sums sha512sums md5sums")
+chksums = valid_chksums + "=(" + ZeroOrMore(opQuotedString(alphanums)) + ")"
 
 # TODO: improve function parsing
-function_body = pypar.nestedExpr(opener="{", closer="}")
+function_body = nestedExpr(opener="{", closer="}")
 build = Literal("build() ") + function_body
+check = Literal("check()") + function_body
+package = Literal("package() ") + function_body
+
+comment = "#" + OneOrMore(Word(printables)) + lineEnd
+
+# TODO: match all possible PKGBUILDs
+pkgbuildline = (pkgname | pkgver | pkgrel | pkgdesc | epoch | url | license
+               | install | changelog | source | noextract | chksums | groups
+               | arch | backup | depends | makedepends | optdepends | conflicts
+               | provides | replaces | options | build | check | package | comment)
+pkgbuildline.addParseAction(lambda x: logging.info(x))
+pkgbuild = OneOrMore(pkgbuildline)
 
